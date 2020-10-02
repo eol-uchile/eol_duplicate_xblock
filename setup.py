95c8454765a344eb92ef1e03c80c492018bac6de
Luchin101 <luis.santanta101@gmail.com>
2020-10-02 12:49:14 -0300

    move file from theme to this app as static files

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
-import setuptools
+import os
 
-setuptools.setup(
+from setuptools import setup
+
+def package_data(pkg, roots):
+    """Generic function to find package_data.
+    All of the files under each of the `roots` will be declared as package
+    data for package `pkg`.
+    """
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+setup(
     name="eol_duplicate_xblock",
-    version="0.0.1",
+    version="0.2",
     author="Felipe Espinoza",
     author_email="felipe.espinoza.r@uchile.cl",
     description="Authentication backend for Chile uchileedxlogin",
     long_description="Authentication backend for Chile uchileedxlogin",
     url="https://eol.uchile.cl",
-    packages=setuptools.find_packages(),
+    packages=['eol_duplicate_xblock'],
     install_requires=["unidecode>=1.1.1"],
     classifiers=[
         "Programming Language :: Python :: 2",
@@ -16,5 +31,8 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     entry_points={
-        "cms.djangoapp": ["eol_duplicate_xblock = eol_duplicate_xblock.apps:EolDuplicateXblockConfig"]},
+        "cms.djangoapp": ["eol_duplicate_xblock = eol_duplicate_xblock.apps:EolDuplicateXblockConfig"],
+        "lms.djangoapp": ["eol_duplicate_xblock = eol_duplicate_xblock.apps:EolDuplicateXblockConfig"]
+    },
+    package_data=package_data("eol_duplicate_xblock", ["static", "public"]),
 )
